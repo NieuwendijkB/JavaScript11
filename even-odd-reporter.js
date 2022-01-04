3496143353925bddd504ec4b6d9d50db14c70364
NieuwendijkB <93547503+NieuwendijkB@users.noreply.github.com>
2022-01-04 16:52:30 +0100

    Add files via upload

---
+++ even-odd-reporter.js
@@ -0,0 +1,17 @@
+for (let i = 1; i <= 20; i++) {
+  if (i % 2 == 0) {
+    console.log(i + "is even");
+  } else {
+    console.log(i + "is oneven");
+  }
+}
+
+// TODO: determine if i is odd or even
+// TODO: use console.log
+
+// Expected output:
+// 1 is oneven
+// 2 is even
+// 3 is oneven
+// 4 is even
+// ...
---
+++ multiplication-tables.js
@@ -0,0 +1,6 @@
+const multiplication = 9;
+
+for (i = 1; i <= 10; i++) {
+  const sum = i * multiplication;
+  console.log(i + " x " + multiplication + " = " + sum);
+}
